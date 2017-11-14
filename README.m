c7d6f7fd9461f79a0d6cc6efdb6b1f04bb8d85a3
Hamza Ikine <hamzaikine@gmail.com>
2017-11-14 17:31:52 -0500

    Update and rename .gitignore to README.m

---
+++ README.m
@@ -0,0 +1,13 @@
+# BitcoinDashboard
+
+A Java application that will allow a user to keep track of information regarding the Bitcoin network.
+
+A user will be able to:
+> View the current $US price per bitcoin
+> Get an up to date price chart for a given time period (1 day, 5 day,etc.),
+  which  will periodically   update   (for   example,   every   15   seconds)
+> View information about specific blocks by providing the block number,  
+  as well as being able to navigate to the previous and next block in the chain
+> Get the current balance of a provided bitcoin address.
+> Store a list of Bitcoin addresses for quick display as well as allow searching for a
+  single address.
