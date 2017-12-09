89a9d93a3d731f9baa30d4cfb0f49ce7556db0cc
Hamza Ikine <hamzaikine@gmail.com>
2017-12-09 15:05:24 -0500

    Update README.m

--- README.m
+++ README.m
@@ -7,7 +7,6 @@ A user will be able to:
 > Get an up to date price chart for a given time period (1 day, 5 day,etc.),
   which  will periodically   update   (for   example,   every   15   seconds)
 > View information about specific blocks by providing the block number,  
-  as well as being able to navigate to the previous and next block in the chain
+  as well as being able to navigate to the previous one in the chain
 > Get the current balance of a provided bitcoin address.
-> Store a list of Bitcoin addresses for quick display as well as allow searching for a
-  single address.
+> searching for a single address.
